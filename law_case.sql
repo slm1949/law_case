eace450ec544a9c6ec52db8d5f9ccd62e3d9e19e
slm1949 <slm1949@163.com>
2013-03-16 23:06:34 +0800

    提交数据库

---
+++ law_case.sql
@@ -0,0 +1,58 @@
+-- phpMyAdmin SQL Dump
+-- version 3.4.11.1deb1
+-- http://www.phpmyadmin.net
+--
+-- 主机: localhost
+-- 生成日期: 2013 年 03 月 16 日 23:04
+-- 服务器版本: 5.5.29
+-- PHP 版本: 5.4.6-1ubuntu1.1
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- 数据库: `law_case`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `think_article`
+--
+
+CREATE TABLE IF NOT EXISTS `think_article` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `case_id` int(11) NOT NULL,
+  `article_name` varchar(50) NOT NULL,
+  `summary` varchar(500) NOT NULL,
+  `content` text NOT NULL,
+  `create_time` datetime NOT NULL,
+  `modify_time` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `think_case`
+--
+
+CREATE TABLE IF NOT EXISTS `think_case` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `case_name` varchar(50) NOT NULL,
+  `describe` varchar(255) NOT NULL,
+  `picture` blob NOT NULL,
+  `create_time` datetime NOT NULL,
+  `modify_time` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
