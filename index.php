4230374b621e5b0b69969eec555918b2d4264b90
Thinking Song <123wuming_123@163.com>
2013-03-17 18:25:00 +0800

    修改为thinkphp推荐部署目录结构

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
 <?php
+define('BUILD_DIR_SECURE',true);
+define('DIR_SECURE_FILENAME', 'index.html');
+define('DIR_SECURE_CONTENT', 'deney Access!');
+//定义项目名称
+define('APP_NAME', 'App');
+//定义项目路径
+define('APP_PATH', './protected/App/');
 define('APP_DEBUG',TRUE); // 开启调试模式
-require '/var/www/ThinkPHP/ThinkPHP.php';
+
+require './protected/ThinkPHP/ThinkPHP.php';
