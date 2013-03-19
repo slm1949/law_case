dec70fa54d4dfcea6cd730f477afc3ca3931ff98
slm1949 <slm1949@163.com>
2013-03-19 21:56:28 +0800

    新增ARTICL的自动验证和自动填充

--- protected/App/Lib/Model/ArticleModel.class.php
+++ protected/App/Lib/Model/ArticleModel.class.php
@@ -1,12 +1,16 @@
 <?php
-  class ArticleModel extends Model{
-    public function read($case_id){
-      $info=M('Artcle');
-      $condition['case_id']=$case_id;
-      $data=$info->where($condition)->order('sort')->select();
-      return $data;
+class ArticleModel extends Model{
+   // 定义自动验证
+    protected $_validate    =   array(
+        array('articlename','require','名称必须填写'),
+    );
+
+    // 定义自动完成
+    protected $_auto    =   array(
+        array('create_time', 'get_client_time' , 1, 'callback'),
+        array('modify_time', 'get_client_time' , 2, 'callback'),
+    );
+    public function get_client_time(){
+        return date("Y-m-d H:i:s");
     }
-    public function write(){
-      
-    }
-  }
+}
