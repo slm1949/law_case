a126e617ec20fb847e22dbd09fe18b5971a3b705
slm1949 <slm1949@163.com>
2013-03-21 23:41:36 +0800

    后台管理员登陆验证，验证部分，SESSION的状态保持，还没做。 #4

---
+++ protected/App/Lib/Action/UserAction.class.php
@@ -0,0 +1,12 @@
+<?php
+  class UserAction extends Action{
+    public function login(){
+      $Form=M('user');
+      $condition['name']=$_POST['user_name'];
+      $condition['password']=$_POST['password'];
+      if($Form->where($condition)->count()>=1){
+       $_SESSION['user_name']=$_POST['user_name'];
+       $this->success('登陆成功', '__APP__/Case/index');
+      }
+    }
+  }    
