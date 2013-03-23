f2c7aff4cd754e9521c0ca9d97f6eef7f7c346ff
slm1949 <slm1949@163.com>
2013-03-23 23:32:27 +0800

    修改文字，添加 修改 删除的跳转错误

--- protected/App/Lib/Action/Admin/ArticleAction.class.php
+++ protected/App/Lib/Action/Admin/ArticleAction.class.php
@@ -20,7 +20,7 @@ class ArticleAction extends Action {
     }
 
     public function add(){
-        $this->case_id=$_GET['id'];
+        $this->case_id=$_GET['case_id'];
         $this->display();
     }
 
@@ -41,6 +41,7 @@ class ArticleAction extends Action {
     public function edit($id=0){
         $Article   =   M('Article');
         $this->article   =   $Article->find($id);
+        $this->case_id=$_GET['case_id'];
         $this->display();
     }
 
@@ -49,7 +50,7 @@ class ArticleAction extends Action {
         if($Article->create()) {
             $result =   $Article->save();
             if($result) {
-                $this->success('操作成功！', '__APP__/Admin/Case/index');
+                $this->success('操作成功！', '__APP__/Admin/Case/read/id/'.$_POST['case_id']);
             }else{
                 $this->error('写入错误！');
             }
@@ -61,7 +62,6 @@ class ArticleAction extends Action {
     public function delete($id=0){
         $Article = M('Article');
         $Article->where("id=$id")->delete();
-
-        $this->success('删除成功', '__APP__/Admin/Case/index');
+        $this->success('删除成功');
     }
 }
