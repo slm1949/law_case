09885fe24330c674a58e9702a7748bb1337515b9
slm1949 <slm1949@163.com>
2013-03-25 00:51:01 +0800

    修改主页到文章的链接错误，及从主页转入文章页面时没有文章显示的问题

--- protected/App/Lib/Action/Home/ArticleAction.class.php
+++ protected/App/Lib/Action/Home/ArticleAction.class.php
@@ -1,23 +1,23 @@
 <?php
 class ArticleAction extends Action {
-    public function index($id=0){
-           
+    public function index($id=0,$case_id=0){         
 
            $Article   =   M('Article');
-           $Articles =   $Article->select();//读取所有文章信息
-           if($Articles) {
-             $this->articles =   $Articles;
+           $Articles =   $Article->where("case_id='$case_id'")->select();//读取所有文章信息
+	if(!empty($id)){
+           if(!$Articles) {
+		$Articles['article_name']='抱歉，没有文章';
            }
-           else{
-             $Articles['article_name']='抱歉，没有文章';
-             $this->articles=$Articles;
-           }
-           $Article_info =   $Article->find($id);//id=$id的文章信息
-           $this->article_info=$Article_info ;
-           $Case = M("Case");
-           //$this->cases = $Case->select();//读取所有案例信息
-           $case_id=$Article_info['case_id'];
-           $this->case_info=$Case->find($case_id);//读取id=case_id 的case信息
-	   $this->display();
+		$Article_info =   $Article->find($id);//id=$id的文章信息
+	}
+	else{
+	$Article_info =   $Article->where("case_id='$case_id'")->find();//id=$id的文章信息
+	}
+	$this->articles=$Articles;
+	$this->article_info=$Article_info ;
+	$Case = M("Case");
+	$this->case_id=$case_id;
+	$this->case_info=$Case->find($case_id);//读取id=case_id 的case信息
+	$this->display();
     }
 }
