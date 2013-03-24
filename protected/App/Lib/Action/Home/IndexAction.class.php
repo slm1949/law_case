b095e383f57778691de1aeebc91474b11e0b337a
slm1949 <slm1949@163.com>
2013-03-24 23:40:42 +0800

    修改主页 案例的前台显示

--- protected/App/Lib/Action/Home/IndexAction.class.php
+++ protected/App/Lib/Action/Home/IndexAction.class.php
@@ -1,23 +1,21 @@
 <?php
 class IndexAction extends Action {
-    public function index($id=0){
-           
-
-           $Article   =   M('Article');
-           $Articles =   $Article->select();//读取所有文章信息
-           if($Articles) {
-             $this->articles =   $Articles;
+    public function index(){
+           $Case   =   M('Case');
+           $Cases =   $Case->select();//读取所有案例信息
+           if($Cases) {
+             $this->cases =   $Cases;
            }
            else{
-             $Articles['article_name']='抱歉，没有文章';
-             $this->articles=$Articles;
-           }
-           $Article_info =   $Article->find($id);//id=$id的文章信息
-           $this->article_info=$Article_info ;
+             $Cases['Case_name']='抱歉，没有案例';
+             $this->Cases=$Cases;
+          }
+ /*           $Case_info =   $Case->find($id);//id=$id的文章信息
+           $this->Case_info=$Case_info ;
            $Case = M("Case");
            //$this->cases = $Case->select();//读取所有案例信息
-           $case_id=$Article_info['case_id'];
-           $this->case_info=$Case->find($case_id);//读取id=case_id 的case信息
+           $case_id=$Case_info['case_id'];
+           $this->case_info=$Case->find($case_id);//读取id=case_id 的case信息*/
 	   $this->display();
-    }
+    }  
 }
