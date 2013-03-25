b9c3b56d77c06fd0d391424ccdc248ef78dc8a5a
Thinking Song <123wuming_123@163.com>
2013-03-25 22:35:57 +0800

    关闭调试模式

--- index.php
+++ index.php
@@ -1,11 +1,10 @@
 <?php
-define('BUILD_DIR_SECURE',true);
+define('BUILD_DIR_SECURE', true);
 define('DIR_SECURE_FILENAME', 'index.html');
 define('DIR_SECURE_CONTENT', 'deney Access!');
 //定义项目名称
 define('APP_NAME', 'App');
 //定义项目路径
 define('APP_PATH', './protected/App/');
-define('APP_DEBUG',TRUE); // 开启调试模式
 
 require './protected/ThinkPHP/ThinkPHP.php';
