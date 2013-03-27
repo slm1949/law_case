7c46e502944eba076668874f966356b29378069c
Thinking Song <123wuming_123@163.com>
2013-03-27 16:07:28 +0800

    修改首页跳转到卢光明案

--- protected/App/Lib/Action/Home/IndexAction.class.php
+++ protected/App/Lib/Action/Home/IndexAction.class.php
@@ -1,21 +1,23 @@
 <?php
 class IndexAction extends Action {
     public function index(){
-       $Case   =   M('Case');
-       $Cases =   $Case->select();//读取所有案例信息
-       if($Cases) {
-         $this->cases =   $Cases;
-       }
-       else{
-         $Cases['Case_name']='抱歉，没有案例';
-         $this->Cases=$Cases;
-      }
+      $this->redirect('Article/index/', array('case_id'=>1));
+
+      //  $Case   =   M('Case');
+      //  $Cases =   $Case->select();//读取所有案例信息
+      //  if($Cases) {
+      //    $this->cases =   $Cases;
+      //  }
+      //  else{
+      //    $Cases['Case_name']='抱歉，没有案例';
+      //    $this->Cases=$Cases;
+      // }
 /*           $Case_info =   $Case->find($id);//id=$id的文章信息
        $this->Case_info=$Case_info ;
        $Case = M("Case");
        //$this->cases = $Case->select();//读取所有案例信息
        $case_id=$Case_info['case_id'];
-       $this->case_info=$Case->find($case_id);//读取id=case_id 的case信息*/
-	   $this->display();
+       $this->case_info=$Case->find($case_id);//读取id=case_id 的case信息 */
+	     // $this->display();
     }  
 }
