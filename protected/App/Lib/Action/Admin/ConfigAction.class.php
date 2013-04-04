b88c2b3065b957f542c5b8620badd19667a34011
slm1949 <slm1949@163.com>
2013-04-04 18:28:43 +0800

    修复配置信息没有权限验证的问题

--- protected/App/Lib/Action/Admin/ConfigAction.class.php
+++ protected/App/Lib/Action/Admin/ConfigAction.class.php
@@ -1,10 +1,11 @@
 <?php
-class ConfigAction extends Action {
+class ConfigAction extends BaseAction {
 	public function index(){
 		$sites=$this->site_info();
 		$this->assign($sites);
 		$this->display();
-	}	
+	}
+
 	public function update(){
 		$site=M('config');
 		$site-> where('`key`="site_name"')->setField('value',"$_POST[site_name]");
@@ -12,8 +13,9 @@ class ConfigAction extends Action {
 		$site-> where('`key`="phone"')->setField('value',"$_POST[site_phone]");
 		$site-> where('`key`="email"')->setField('value',"$_POST[site_email]");
 		$this->success('修改网站信息成功');
-	}	
-	public function site_info(){	
+	}
+
+	public function site_info(){
 		$site=M('config');
 		$sites['site_name']=$site->where('`key`="site_name"')->getField('value');
 		$sites['site_description']=$site->where("`key`='site_description'")->getField('value');
