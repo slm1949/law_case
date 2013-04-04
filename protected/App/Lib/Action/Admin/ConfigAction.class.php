2d02b677f39bbaa93385536c67757aaa8c770ce7
slm1949 <slm1949@163.com>
2013-04-04 22:58:35 +0800

    移动加载网站配置到MODEL

--- protected/App/Lib/Action/Admin/ConfigAction.class.php
+++ protected/App/Lib/Action/Admin/ConfigAction.class.php
@@ -1,7 +1,8 @@
 <?php
 class ConfigAction extends BaseAction {
 	public function index(){
-		$sites=$this->site_info();
+	    $config=D('Config');
+		$sites=$config->map();
 		$this->assign($sites);
 		$this->display();
 	}
@@ -10,17 +11,8 @@ class ConfigAction extends BaseAction {
 		$site=M('config');
 		$site-> where('`key`="site_name"')->setField('value',"$_POST[site_name]");
 		$site-> where('`key`="site_description"')->setField('value',"$_POST[site_description]");
-		$site-> where('`key`="phone"')->setField('value',"$_POST[site_phone]");
-		$site-> where('`key`="email"')->setField('value',"$_POST[site_email]");
+		$site-> where('`key`="site_phone"')->setField('value',"$_POST[site_phone]");
+		$site-> where('`key`="site_email"')->setField('value',"$_POST[site_email]");
 		$this->success('修改网站信息成功');
-	}
-
-	public function site_info(){
-		$site=M('config');
-		$sites['site_name']=$site->where('`key`="site_name"')->getField('value');
-		$sites['site_description']=$site->where("`key`='site_description'")->getField('value');
-		$sites['site_phone']=$site->where("`key`='phone'")->getField('value');
-		$sites['site_email']=$site->where("`key`='email'")->getField('value');
-		return $sites;
 	}
 }
--- protected/App/Lib/Action/Home/IndexAction.class.php
+++ protected/App/Lib/Action/Home/IndexAction.class.php
@@ -1,25 +1,25 @@
 <?php
 class IndexAction extends Action {
     public function index(){
-      //$this->redirect('Article/index/', array('case_id'=>1));
-	$config=A('Admin/Config');
-	$sites=$config->site_info();
-	$this->assign($sites);//获取网站的配置信息    
-	import('ORG.Util.Page');// 导入分页类
+        $config=D('Config');
+        $sites=$config->map();
+        var_dump($sites);
+        $this->assign($sites);//获取网站的配置信息
+        import('ORG.Util.Page');// 导入分页类
         $Case   =   M('Case');
-	$count      = $Case->count();// 查询满足要求的总记录数
-	$Page       = new Page($count,8);// 实例化分页类,传入总记录数和每页的记录数
-	$show       = $Page->show();// 分页显示输出
-	$this->assign('page',$show);// 赋值分页输出
-	// 进行分页数据查询
+        $count      = $Case->count();// 查询满足要求的总记录数
+        $Page       = new Page($count,8);// 实例化分页类,传入总记录数和每页的记录数
+        $show       = $Page->show();// 分页显示输出
+        $this->assign('page',$show);// 赋值分页输出
+        // 进行分页数据查询
         $Cases =   $Case->limit($Page->firstRow.','.$Page->listRows)->select();//读取所有案例信息
         if($Cases) {
-          $this->cases =   $Cases;
+            $this->cases =   $Cases;
         }
         else{
-          $Cases['Case_name']='抱歉，没有案例';
-          $this->Cases=$Cases;
-       }
-	      $this->display();
-    }  
+            $Cases['Case_name']='抱歉，没有案例';
+            $this->Cases=$Cases;
+        }
+        $this->display();
+    }
 }
