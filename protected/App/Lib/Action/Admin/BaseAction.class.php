6d946442f8d560f8b0f9d234b2ae6728b8ebdf9b
slm1949 <slm1949@163.com>
2013-04-09 00:53:44 +0800

    增加密码修改

--- protected/App/Lib/Action/Admin/BaseAction.class.php
+++ protected/App/Lib/Action/Admin/BaseAction.class.php
@@ -7,6 +7,13 @@ class BaseAction extends Action {
         }else{
             $this->assign(ACTION_NAME.'_active','class="active"');//获得当前模块的名，并激活当前模块对应的导航栏
         }
+        if(!empty($_SESSION['user_name'])){
+            if(ACTION_NAME=='change_pass'){
+                $this->assign('change_password','<li class="active"><a href="__APP__/Admin/User/change_pass">修改密码</a></li>');
+            }else{
+                $this->assign('change_password','<li><a href="__APP__/Admin/User/change_pass">修改密码</a></li>');
+            }
+        }
     }
 
     protected function verify(){
--- protected/App/Lib/Action/Admin/UserAction.class.php
+++ protected/App/Lib/Action/Admin/UserAction.class.php
@@ -21,4 +21,25 @@ class UserAction extends BaseAction{
         session('user_name', null);
         $this->success('管理员注销成功', 'login');
     }
+
+    public function change_pass(){
+        $this->display();
+    }
+
+    public function update_pass(){
+        $user=M('User');
+        $key=$user->where(array('user_name' => $_POST['user_name_old'],'password' => md5($_POST['password_old'])))->getField('id');
+        if(!$key){
+            $this->error('用户名或密码错误');
+        }else{
+            if($_POST['password']==$_POST['password_repeat']){
+                $user_info['user_name']=$_POST['user_name'];
+                $user_info['password']=md5($_POST['password']);
+                $user->where(array('id' => $key))->save($user_info);
+                $this->success('用户名密码修改成功');
+            }else{
+                $this->error('两次输入的新密码不一致');
+            }
+        }
+    }
 }    
