0885fef7e5b52900184ea5933cb205008d30b4a5
slm1949 <slm1949@163.com>
2013-04-09 21:43:07 +0800

    导航栏跟随URL变化改在模板里判断

--- protected/App/Lib/Action/Admin/BaseAction.class.php
+++ protected/App/Lib/Action/Admin/BaseAction.class.php
@@ -1,19 +1,8 @@
 <?php
 class BaseAction extends Action {
     public function  _initialize(){
-        if(MODULE_NAME!='User'){
-            $this->assign(MODULE_NAME.'_active','class="active"');//获得当前模块的名，并激活当前模块对应的导航栏
+        if(MODULE_NAME!='User')
             $this->verify();
-        }else{
-            $this->assign(ACTION_NAME.'_active','class="active"');//获得当前模块的名，并激活当前模块对应的导航栏
-        }
-        if(!empty($_SESSION['user_name'])){
-            if(ACTION_NAME=='change_pass'){
-                $this->assign('change_password','<li class="active"><a href="__APP__/Admin/User/change_pass">修改密码</a></li>');
-            }else{
-                $this->assign('change_password','<li><a href="__APP__/Admin/User/change_pass">修改密码</a></li>');
-            }
-        }
     }
 
     protected function verify(){
